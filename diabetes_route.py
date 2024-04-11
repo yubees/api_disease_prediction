5642c59e464c5fd2452ef791f30d41e439ff965a
yube01 <yubraja008@gmail.com>
2024-04-11 13:36:39 +0545

    initial file for glitch

---
+++ diabetes_route.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, request,jsonify
+import joblib
+import numpy as np
+
+heart_api = Blueprint('heart_api',__name__)
+
+model = joblib.load("./final_diabetes_model.joblib")
+
+
+# Register route
+@heart_api.route('/heart_disease', methods=['POST'])
+def diabetes_check():
+    try:
+        # Extract data from request
+        data = request.json
+        age = data.get('age')
+        sex = data.get('sex')
+        cp = data.get('cp')
+        chol = data.get('chol')
+        fbs = data.get('fbs')
+        restecg = data.get('restecg')
+        thalach = data.get("thalach")
+        exang = data.get("exang")
+        oldpeak = data.get('oldpeak')
+        slope = data.get("slope")
+        ca = data.get("ca")
+        thal = data.get("thal")
+
+
+        input_data = (age,sex,cp,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal)
+        input_data_as_numpy_array = np.asarray(input_data)
+
+        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+        prediction = model.predict(input_data_reshaped)
+
+
+
+        if (prediction[0]== 0):
+            print('The Person does not have a Heart Disease')
+        else:
+            print('The Person has Heart Disease')
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
