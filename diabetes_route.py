43b1a6864e64f2cc65b3f68008dc8dee83d8da04
yube01 <yubraja008@gmail.com>
2024-04-11 14:07:21 +0545

    fixed

--- diabetes_route.py
+++ diabetes_route.py
@@ -2,43 +2,41 @@ from flask import Blueprint, request,jsonify
 import numpy as np
 import pickle
 
-heart_api = Blueprint('heart_api',__name__)
 
-model = pickle.load( open("diabetes_model.sav", 'rb'))
+diabetes_api = Blueprint('diabetes_api',__name__)
+model = pickle.load(open("diabetes_model.sav", 'rb'))
 
 
 # Register route
-@heart_api.route('/heart_disease', methods=['POST'])
+@diabetes_api.route('/diabetes', methods=['POST'])
 def diabetes_check():
     try:
         # Extract data from request
         data = request.json
         age = data.get('age')
-        sex = data.get('sex')
-        cp = data.get('cp')
-        chol = data.get('chol')
-        fbs = data.get('fbs')
-        restecg = data.get('restecg')
-        thalach = data.get("thalach")
-        exang = data.get("exang")
-        oldpeak = data.get('oldpeak')
-        slope = data.get("slope")
-        ca = data.get("ca")
-        thal = data.get("thal")
+        hypertension = data.get('hypertension')
+        heart_disease = data.get('heart_disease')
+        bmi = data.get('bmi')
+        hba1c_level = data.get('hba1c_level')
+        glucose_level = data.get('glucose_level')
+        gender = data.get("gender")
+        smoking_history = data.get("smoking_history")
 
-
-        input_data = (age,sex,cp,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal)
+        input_data = (age,hypertension,heart_disease,bmi,hba1c_level,glucose_level,gender,smoking_history)
         input_data_as_numpy_array = np.asarray(input_data)
 
         input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
         prediction = model.predict(input_data_reshaped)
 
-
-
-        if (prediction[0]== 0):
-            print('The Person does not have a Heart Disease')
+        if (prediction[0] == 0):
+          
+            return jsonify({'message': 'The person is not diabetic'}), 201
         else:
-            print('The Person has Heart Disease')
+            
+            return jsonify({'message': 'The person is diabetic'}), 201
+
+
+        return jsonify({'message': 'User created successfully'}), 201
     except Exception as e:
         return jsonify({'error': str(e)}), 500
